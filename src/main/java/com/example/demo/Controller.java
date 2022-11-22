f2194b1e259369a0fc46e813ea8eebac5ebc9301
ruland39 <hcyrf1@nottingham.edu.my>
2022-11-22 22:30:24 +0800

    edit mainmenu.fxml

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,9 +3,7 @@ package com.example.demo;
 import javafx.scene.input.KeyCode;
 
 public class Controller {
-    private Cell cell;
-    private GameScene gameScene;
-    private KeyCode keyInput;
+
 
 
 
