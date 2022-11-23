ba980e56adb7418df12a3a3d1ca907b44d8f9d9f
ruland39 <hcyrf1@nottingham.edu.my>
2022-11-23 22:13:43 +0800

    implemented retry button

--- src/main/java/com/example/game2048/EndGame.java
+++ src/main/java/com/example/game2048/EndGame.java
@@ -1,18 +1,26 @@
 package com.example.game2048;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.Optional;
+
+import static com.example.game2048.Main.HEIGHT;
+import static com.example.game2048.Main.WIDTH;
 
 
 public class EndGame {
@@ -27,18 +35,85 @@ public class EndGame {
     }
 
     public void endGameShow(Scene endGameScene, Group root, Stage primaryStage,long score){
+        //Game Over Text
         Text text = new Text("GAME OVER");
         text.relocate(250,250);
         text.setFont(Font.font(80));
         root.getChildren().add(text);
 
-
+        //Score Text
         Text scoreText = new Text(score+"");
         scoreText.setFill(Color.BLACK);
         scoreText.relocate(350,400);
         scoreText.setFont(Font.font(80));
         root.getChildren().add(scoreText);
 
+
+        //Retry/Play Again Button
+        Button retryButton = new Button("Retry");
+        retryButton.setPrefSize(100,30);
+        retryButton.setTextFill(Color.BLACK);
+        root.getChildren().add(retryButton);
+        retryButton.relocate(400,600);
+        retryButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            private Scene gameScene;
+            private Group gameRoot;
+
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                root.getChildren().clear();
+
+                Group menuRoot = new Group();
+                Scene menuScene = new Scene(menuRoot, WIDTH, HEIGHT);
+                Group accountRoot = new Group();
+                Scene accountScene = new Scene(accountRoot, WIDTH, HEIGHT, Color.rgb(150, 20, 100, 0.2));
+                Group getAccountRoot = new Group();
+                Scene getAccountScene = new Scene(getAccountRoot, WIDTH, HEIGHT, Color.rgb(200, 20, 100, 0.2));
+                Group endgameRoot = new Group();
+                Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
+                Group rankRoot = new Group();
+                Scene rankScene = new Scene(rankRoot, WIDTH, HEIGHT, Color.rgb(250, 50, 120, 0.3));
+                BackgroundFill background_fill = new BackgroundFill(Color.rgb(120, 100, 100), CornerRadii.EMPTY, Insets.EMPTY);
+                Background background = new Background(background_fill);
+
+
+                Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
+                backgroundOfMenu.setX(WIDTH / 2 - 120);
+                backgroundOfMenu.setY(180);
+                menuRoot.getChildren().add(backgroundOfMenu);
+
+                Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
+                backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
+                backgroundOfMenuForPlay.setY(180);
+                accountRoot.getChildren().add(backgroundOfMenuForPlay);
+
+                Group gameRoot = new Group();
+                setGameRoot(gameRoot);
+                Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
+                setGameScene(gameScene);
+                primaryStage.setScene(gameScene);
+                GameScene game = new GameScene();
+                game.game(gameScene, gameRoot, primaryStage, endGameScene, endgameRoot);
+
+                primaryStage.show();
+                
+                
+            }
+
+            private void setGameScene(Scene gameScene) {
+                this.gameScene = gameScene;
+            }
+
+            private void setGameRoot(Group gameRoot) {
+                this.gameRoot = gameRoot;
+            }
+        });
+
+
+
+
+
+        //Quit Button
         Button quitButton = new Button("QUIT");
         quitButton.setPrefSize(100,30);
         quitButton.setTextFill(Color.BLACK);
